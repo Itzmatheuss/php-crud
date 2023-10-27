eecec9a8b93ac915ce4f509cd4744d8d973ec694
itzmatheuss <matheusp.ss199@gmail.com>
2023-10-27 13:33:51 -0300

    conteudo

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Student Create</title>
+</head>
+<body>
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
